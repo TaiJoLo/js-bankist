50b434cc711946dc6b82e8c1dd4c45e1a64a9dba
LOLO <lotaijo@gmail.com>
2023-02-19 19:01:55 +1300

    Efficient Script Loading: defer in head

--- starter/script.js
+++ starter/script.js
@@ -277,3 +277,19 @@ const slider = function () {
   });
 };
 slider();
+
+// Lifecycle DOM event
+
+document.addEventListener('DOMContentLoaded', function (e) {
+  console.log('HTML parsed and DOM tree built!', e);
+});
+
+window.addEventListener('load', function (e) {
+  console.log('Page fully loaded!', e);
+});
+
+// window.addEventListener('beforeunload', function (e) {
+//   e.preventDefault();
+//   console.log(e);
+//   e.returnValue = '';
+// });
